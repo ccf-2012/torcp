bace3c49ccf09eec07424218db85bc728c2873c6
ccf2012 <crlf66@163.com>
2021-12-01 20:30:05 +0800

    pass test

--- test_parseTorName.py
+++ test_parseTorName.py
@@ -1,5 +1,6 @@
 import pytest
 import torcp
+from torguess import GuessCategoryUtils
 
 
 @pytest.mark.parametrize("test_input, e1, e2, e3", [
@@ -30,29 +31,50 @@ import torcp
     ('CCTV1.Yuan.Meng.Zhong.Guo.De.Yao.Zhong.Hua.20211129.HDTV.1080i.H264-HDSTV.ts',
      'Yuan Meng Zhong Guo De Yao Zhong Hua 20211129', '', ''),
     ('CCTV5+.2021.Snooker.UK.Championship.20211129.HDTV.1080i.H264-HDSTV.ts',
-     '2021 Snooker UK Championship 20211129', '', ''),
+     '2021 Snooker UK Championship 20211129', '2021', ''),
     ('CCTV5+.2021.World.Table.Tennis.Championship.20211130.HDTV.1080i.H264-HDSTV.ts',
-     '2021 World Table Tennis Championship 20211130', '', ''),
-    ('CCTV5.Total.Soccer.20211129.HDTV.1080i.H264-HDSTV.ts', 'Total Soccer 20211129',
-     '', ''),
+     '2021 World Table Tennis Championship 20211130', '2021', ''),
+    ('CCTV5.Total.Soccer.20211129.HDTV.1080i.H264-HDSTV.ts',
+     'Total Soccer 20211129', '', ''),
     ('CCTV9.The.Legend.Of.Film.Ep01-Ep06.HDTV.1080i.H264-HDSTV',
-     'The Legend Of Film', '', ''),
+     'The Legend Of Film', '', 'Ep01-Ep06'),
     ('The.Boys.S02.2020.1080p.BluRay.DTS.x265-10bit-HDS', 'The Boys', '2020',
      'S02'),
+    ('Doctor.X.Surgeon.Michiko.Daimon.S06.1080p.BluRay.x265.10bit.FLAC.MNHD-FRDS',
+     'Doctor X Surgeon Michiko Daimon', '', 'S06'),
+    ('逆世界.Upside.Down.2012.BluRay.1080p.x265.10bit.2Audio.MNHD-FRDS',
+     'Upside Down', '2012', ''),
+    ('野战排.Platoon.1986.BluRay.1080p.x265.10bit.2Audio.MNHD-FRDS', 'Platoon',
+     '1986', ''),
+    ('Stargate.Atlantis.S04.Multi.1080p.BluRay.DTS-HDMA.5.1.H.264-CELESTIS',
+     'Stargate Atlantis', '', 'S04'),
+    ('谍影重重1-5.The.Bourne.2002-2016.1080p.Blu-ray.x265.DTS￡cXcY@FRDS',
+     'The Bourne', '2002-2016', ''),
+    ('豹.1963.JPN.1080p.意大利语中字￡CMCT风潇潇', '豹', '1963', ''),
+    ('过界男女.2013.国粤双语.简繁中字￡CMCT紫卿醺', '过界男女', '2013', ''),
+    ('金刚狼3殊死一战.Logan.2017.BluRay.1080p.x265.10bit.MNHD-FRDS', 'Logan', '2017',
+     ''),
+    ('(BDMV)Anneke Gronloh - De Regenboog Serie (2009) FLAC-CD] {NL,Telstar B.V,TCD 70316-2}',
+     'Anneke Gronloh - De Regenboog Serie', '2009', ''),
+    ('Foundation.2021.S01.2160p.ATVP.WEB-DL.DDP5.1.DV.HEVC-CasStudio',
+     'Foundation', '2021', 'S01'),
+    ('我是你的眼.I\'m.Your.Eyes.2016.S01.2160p.WEB-DL.H265.AAC-LeagueWEB',
+     'I\'m Your Eyes', '2016', 'S01'),
 ])
 def test_parseTVName(test_input, e1, e2, e3):
-    a1, a2, a3 = torcp.parseMovieName(test_input)
+    a1, a2, a3, a4 = torcp.parseMovieName(test_input)
     assert a1 == e1 and a2 == e2 and a3 == e3
-    # assert (a1==e1 and a2==e2 and a3==e3)
 
 
-# def test_parseTVName():
-# ss = [
-#     ('权力的游戏.第S01-S08.Game.Of.Thrones.S01-S08.1080p.Blu-Ray.AC3.x265.10bit-Yumi',
-#      'Game Of Thrones', '', 'S01-S08'),
-#     ('辅佐官：改变世界的人们S01-S02.Chief.of.Staff.2019.1080p.WEB-DL.x265.AC3￡cXcY@FRDS',
-#      'Chief of Staff', '2019', 'S01-S02'),
-
-# ]
-# for s in ss:
-#     assert torcp.parseMovieName(s[0]) == (s[1], s[2], s[3])
+@pytest.mark.parametrize("test_input, e1, e2", [
+    ('权力的游戏.第S01-S08.Game.Of.Thrones.S01-S08.1080p.Blu-Ray.AC3.x265.10bit-Yumi',
+     'TV', 'YUMI'),
+    ('辅佐官：改变世界的人们S01-S02.Chief.of.Staff.2019.1080p.WEB-DL.x265.AC3￡cXcY@FRDS',
+     'TV', 'FRDS'),
+    ('半暖时光.The.Memory.About.You.S01.2021.2160p.WEB-DL.AAC.H265-HDSWEB', 'TV',
+     'HDSWEB'),
+    ('不惑之旅.To.the.Oak.S01.2021.2160p.WEB-DL.AAC.H265-HDSWEB', 'TV', 'HDSWEB'),
+])
+def test_guessByName(test_input, e1, e2):
+    a1, a2 = GuessCategoryUtils.guessByName(test_input)
+    assert a1 == e1 and a2 == e2
--- torcp.py
+++ torcp.py
@@ -7,6 +7,7 @@ from torguess import GuessCategoryUtils
 GD_CONFIG = r'/root/.config/rclone/rclone.conf'
 GD_PATH = r'gd123:/176/'
 
+
 def parseMovieName(torName):
     sstr = GuessCategoryUtils.cutExt(torName)
 
@@ -26,35 +27,53 @@ def parseMovieName(torName):
     }
     for original, replacement in dilimers.items():
         sstr = sstr.replace(original, replacement)
-    sstr = re.sub(r'(BDMV|BDRemux|\bCCTV\d)\W*', '', sstr, flags=re.I)
-    sstr = re.sub(r'Ep?\d+-Ep?\d+\b.*$', '', sstr, flags=re.I)
-    sstr = re.sub(r' +', ' ', sstr).strip()
-    seasonstr = ''
-    sstr = re.sub(r'\S+\w+@\w*', '', sstr)
-    m = re.search(r'\bS\d+(-S\d+)?\b', sstr, flags=re.A|re.I)
-    if m:
-        seasonstr = m.group(0)
-        sstr = sstr.replace(seasonstr, '')
-    yearstr = ''
-    m = re.search(r'\w+.*\b(19\d{2}\b|20\d{2})\b.*$', sstr, flags=re.A|re.I)
-    if m:
-        yearstr = m.group(1)
-        sstr = re.sub(yearstr+'.*$', '', sstr)
-        # sstr = sstr.replace(yearstr, '')
 
-    chtitle = sstr
-    m = re.search(
-        r'^.*[\u4e00-\u9fa5\u3041-\u30fc](S\d+| |\.|\d|-)*(?=[A-Z])', sstr)
+    sstr = re.sub(r'^\W?(BDMV|\BDRemux|\bCCTV\d)\W*', '', sstr, flags=re.I)
+    # sstr = re.sub(r'\S+\w+@\w*', '', sstr)
+    seasonstr = ''
+    yearstr = ''
+    titlestr = sstr
+    mep = re.search(r'(\bEp?\d+-Ep?\d+)\b', sstr, flags=re.A | re.I)
+    if mep:
+        seasonstr = mep.group(1)
+        sstr = sstr.replace(seasonstr, '')
+    m1 = None
+    for m1 in re.finditer(r'(\bS\d+(-S\d+)?)\b', sstr, flags=re.A | re.I):
+        pass
+    # m1 = re.search(r'(\bS\d+(-S\d+)?)\b', sstr, flags=re.A | re.I)
+    if m1:
+        seasonstr = m1.group(1)
+        seasonsapn = m1.span(1)
+        sstr = sstr.replace(seasonstr, '')
+
+    m2 = re.search(r'\b((19\d{2}\b|20\d{2})-?(19\d{2}|20\d{2})?)\b',
+                   sstr,
+                   flags=re.I)
+    if m2:
+        yearstr = m2.group(1)
+        yearspan = m2.span(1)
+        if re.search(r'\w.*' + yearstr, sstr):
+            sstr = sstr[:yearspan[0] - 1]
+    else:
+        if m1:
+            ss2 = sstr[:seasonsapn[0] - 1]
+            if not re.search(r'[\u4e00-\u9fa5\u3041-\u30fc]$', ss2):
+                sstr = ss2
+
+    titlestr = re.sub(r' +', ' ', sstr).strip()
+
+    chtitle = titlestr
+    m = re.search(r'^.*[\u4e00-\u9fa5\u3041-\u30fc](S\d+| |\.|\d|-)*(?=[A-Z])',
+                  titlestr)
     if m:
         chtitle = m.group(0)
-        sstr = sstr.replace(chtitle, '')
-    sstr = re.sub(r'\((\w+| )\)?(?!.*\(.*\)).*$', '', sstr).strip()
-    if sstr.endswith(' JP'):
-        sstr = sstr.replace(' JP', '')
+        titlestr = titlestr.replace(chtitle, '')
 
-    titlestr = sstr if len(sstr) > 6 else chtitle
-    return titlestr, yearstr, seasonstr
-    # return titlestr
+    # titlestr = re.sub(r'\((\w+| )\)?(?!.*\(.*\)).*$', '', sstr).strip()
+    # if titlestr.endswith(' JP'):
+    #     titlestr = titlestr.replace(' JP', '')
+
+    return titlestr, yearstr, seasonstr, chtitle
 
 
 def genTargetDir(cat, filename):
@@ -62,17 +81,23 @@ def genTargetDir(cat, filename):
 
 
 def rcloneCopy(fromLoc, toLoc):
-    print('rclone copy ', fromLoc, GD_PATH+toLoc)
+    print('rclone copy ', fromLoc, GD_PATH + toLoc)
 
     cfg_path = GD_CONFIG
 
     with open(cfg_path) as f:
         cfg = f.read()
 
-    result = rclone.with_config(cfg).copy(fromLoc, GD_PATH+toLoc)
+    result = rclone.with_config(cfg).copy(fromLoc, GD_PATH + toLoc)
 
     return result
 
+def getSeasonFromFolderName(folderName, failDir=''):
+    m1 = re.search(r'(\bS\d+(-S\d+)?)\b', folderName, flags=re.A | re.I)
+    if m1:
+        return m1.group(1)
+    else:
+        return failDir
 
 def main():
     if len(sys.argv) <= 1:
@@ -80,24 +105,29 @@ def main():
     else:
         cpLocation = sys.argv[1]
 
-    for fileItem in os.listdir(cpLocation):
-        cat, group = GuessCategoryUtils.guessByName(fileItem)
-        title, year, season = parseMovieName(fileItem)
-        folderName = title + ' ('+year+')'
+    for torFolderItem in os.listdir(cpLocation):
+        cat, group = GuessCategoryUtils.guessByName(torFolderItem)
+        title, year, season, chtitle = parseMovieName(torFolderItem)
+        mediaFolderName = title + ' (' + year + ')'
         if cat == 'TV':
-            if len(season) > 0:
-                targetDirParent = os.path.join(cat, folderName, season)
-            else:
-                targetDirParent = os.path.join(cat, folderName)
-            tvPath = os.path.join(cpLocation, fileItem)
-            for tvItem in os.listdir(tvPath):
-                tvSource = os.path.join(tvPath, tvItem)
-                if os.path.isdir(tvSource):
-                    rcloneCopy(tvSource, targetDirParent)
+            tv0Path = os.path.join(cpLocation, torFolderItem)
+            for tv1item in os.listdir(tv0Path):
+                tv1FullPath = os.path.join(tv0Path, tv1item)
+                if os.path.isdir(tv1FullPath):
+                    seasonFolder = getSeasonFromFolderName(tv1item, failDir=season)
+                    seasonFolderFullPath = os.path.join(cat, mediaFolderName, seasonFolder)
+                    for tv2item in os.listdir(tv1FullPath):
+                        tv2FullPath = os.path.join(tv1FullPath, tv2item)
+                        rcloneCopy(tv2FullPath, seasonFolderFullPath)
                 else:
-                    rcloneCopy(tvSource, targetDirParent)
-        else:
-            pass                    
+                    seasonFolderFullPath = os.path.join(cat, mediaFolderName, season)
+                    rcloneCopy(tv1FullPath, seasonFolderFullPath )
+        elif cat == 'MovieEncode':
+            movieParentDir = os.path.join(cat, mediaFolderName)
+            movieSource = os.path.join(cpLocation, torFolderItem)
+            for movieItem in os.listdir(movieSource):
+                movieFullPath = os.path.join(movieSource, movieItem)
+                rcloneCopy(movieFullPath, movieParentDir)
 
 
 if __name__ == '__main__':
