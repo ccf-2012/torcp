7699797049fcb6c40294730af665c7791248e070
ccf2012 <crlf66@163.com>
2022-02-20 20:29:41 +0800

    re.sub(r'[\[\(](

--- tortitle.py
+++ tortitle.py
@@ -257,7 +257,7 @@ def parse0DayMovieName(torName):
     sstr = re.sub(r'\[Vol.*\]$', '', sstr, flags=re.I)
 
     sstr = re.sub(r'\W?(IMAX|Extended Cut|\d+CD|APE整轨)\b.*$', '', sstr, flags=re.I)
-    sstr = re.sub(r'[[(](BD\d+|WAV\d*|(CD\-)?FLAC|Live|DSD\s?\d*)\b.*$', '', sstr, flags=re.I)
+    sstr = re.sub(r'[\[\(](BD\d+|WAV\d*|(CD\-)?FLAC|Live|DSD\s?\d*)\b.*$', '', sstr, flags=re.I)
     sstr = re.sub(r'^\W?(BDMV|\BDRemux|\bCCTV\d(HD)?|BD\-?\d*|[A-Z]{1,5}TV)\W*', '', sstr, flags=re.I)
 
     sstr = re.sub(r'\{[^\}]*\}.*$', '', sstr, flags=re.I)
