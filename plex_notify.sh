054b5fdf79bd7d716460408c53b17e8f49c5f835
ccf2012 <crlf66@163.com>
2022-07-21 13:59:56 +0800

    plex_notify.sh

--- plex_notify.sh
+++ plex_notify.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 #
-echo '$1'
+echo "$1"
 
-# python3 /home/ccf2012/plex_notify/plex_notify.py -l 2 -f '$1' -s http://plex.server.ip:32400 -t plex-token
+# python3 /home/ccf2012/plex_notify/plex_notify.py -l 2 -f "$1" -s http://plex.server.ip:32400 -t plex-token
